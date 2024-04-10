7a000d60514a510f77c4278bb6878a83f14fc6d4
Priyanshu Bagasi <125460610+Priyanshu-Bagasi@users.noreply.github.com>
2024-04-10 22:15:05 +0530

    Delete Portfolio Website directory

--- Portfolio Website/index.js
+++
@@ -1,9 +0,0 @@
-let navbar=document.getElementsByClassName("navbar");
-window.addEventListener("scroll",()=>{
-    if(window.screenY>=100){
-        navbar.classList.add("navbar_black")
-    }
-    else{
-        navbar.classList.remove("navbar_black")
-    }
-})
